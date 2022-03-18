a40f085841d057d9ad85dad74f8669c0a5c19a11
jesusmb230795 <jesusmiranda2307@gmail.com>
2022-03-17 20:26:22 -0600

    Se modifica setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bellmand_ford",
+    version="0.0.1",
+    author="Equipo",
+    description="Método numérico que resuelva problemas de optimización convexa de pequeña escala.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/optimizacion-2-2022-gh-classroom/practica-1-segunda-parte-pautrejo",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9.5",
+)
