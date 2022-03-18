62085891cb8261a431c56ea4a9ab2d8e87667927
jesusmb230795 <jesusmiranda2307@gmail.com>
2022-03-17 21:14:36 -0600

    Se corrige nombre del paquete

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="bellmand_ford",
+    name="bellman_ford",
     version="0.0.1",
     author="Equipo",
     description="Método numérico que resuelva problemas de optimización convexa de pequeña escala.",
