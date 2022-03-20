ea899ec772a1b1e19fe2fe3b24b6ce3a82788ab4
jesusmb230795 <jesusmiranda2307@gmail.com>
2022-03-20 13:30:57 -0600

    Se actualiza la función bf

--- src/bellman_ford/bellman_ford.py
+++ src/bellman_ford/bellman_ford.py
@@ -20,20 +20,25 @@ def bf_negative_cycle(graph):
     -------
     https://en.wikipedia.org/wiki/Bellman–Ford_algorithm
     https://nbviewer.org/github/rcroessmann/sharing_public/blob/master/arbitrage_identification.ipynb
+    https://www.simplilearn.com/tutorials/data-structure-tutorial/bellman-ford-algorithm
     """
 
     n_nodes = len(graph.nodes())
     n = n_nodes + 1
     # Remove nan edges
-    edges = [edge for edge in graph.edges().data() if ~np.isnan(edge[2]['weight'])]
+    edges = []
+    for edge in graph.edges().data():
+        if ~np.isnan(edge[2]['weight']):
+            edges.append(edge)
 
     # Add a starting node and add edges with zero weight to all other nodes
-    edges = edges + [(n_nodes, i, {'weight': 0}) for i in range(n_nodes)]
+    for i in range(n_nodes):
+        edges.append((n_nodes, i, {'weight': 0}))
 
     # Initialize node distances and predecessors
     distance= np.ones(n) * np.inf
     distance[n_nodes] = 0  # Starting node has zero distance
-    p = np.ones(n) * -1
+    predecessors = np.ones(n) * -1
 
     # Relax n times
     for i in range(n):  
@@ -41,19 +46,21 @@ def bf_negative_cycle(graph):
         for edge in edges:
             if distance[int(edge[0])] + edge[2]['weight'] < distance[int(edge[1])]:
                 distance[int(edge[1])] = distance[int(edge[0])] + edge[2]['weight']
-                p[int(edge[1])] = int(edge[0])
+                predecessors[int(edge[1])] = int(edge[0])
                 x = int(edge[1])
         if x == -1:  # If no relaxation possible, no negative cycle
             return None
         
     # Identify negative cycle
     for i in range(n):
-        x = p[int(x)]
+        x = predecessors[int(x)]
     cycle = []
     v = x
     while True:
         cycle.append(int(v))
         if v == x and len(cycle) > 1:
             break
-        v = p[int(v)]
-    return list(reversed(cycle))
+        v = predecessors[int(v)]
+    
+    cycle.reverse()
+    return cycle
